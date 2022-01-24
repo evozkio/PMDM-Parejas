8dfdbd0600cd1275f9e68dd9f953736723855818
SolisVazquezFrancisco <franciscojavier.ma41015@cesurformacion.com>
2022-01-24 19:08:59 +0100

    Arreglando estilos y validacion

--- proyecto/src/containers/SubirFrutas.js
+++ proyecto/src/containers/SubirFrutas.js
@@ -11,53 +11,49 @@ function SubirFrutas({navigation}){
   const [price, setPrice] = useState(null);
 
   const onPress = () => {
+    console.log(fruit)
     console.log(price)
     if(isNaN(price)){
       console.log("estoy")
-      Alert.alert("Fruta la fruta no se puede añadir")
+      Alert.alert("La fruta no se puede añadir")
     }else{
-        console.log("noestoy")
-        {SubirFruta}
+      fetch('http://10.0.2.2:8080/fruits', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "name": fruit,
+          "price": price
+        }),
+      })
+      .then((responseJson) => {
+        console.log('getting data from fectch', responseJson);
+        Alert.alert("Fruta añadida correctamente")
+        setFruit(null);
+        setPrice(null);
+      })
+      .catch(error => console.log(error));
     }
-  }
-
-  const SubirFruta = () => {
-    fetch('http://10.0.2.2:8080/fruits', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "name": fruit,
-        "price": price
-      }),
-    })
-    .then((responseJson) => {
-      console.log('getting data from fectch', responseJson);
-      Alert.alert("Fruta añadida correctamente")
-      setFruit(null);
-      setPrice(null);
-    })
-    .catch(error => console.log(error));
   }
 
   return(
     <View>
-        <Text style={styles.c}>Añadir Frutas</Text>
+        <Text style={styles.titulos}>Añadir Frutas</Text>
         
-            <Text style={styles.c}>Nombre de la Fruta</Text>
+            <Text style={styles.titulos}>Nombre de la Fruta</Text>
           
-            <ModalDropdown style={styles.mdd2} textStyle={styles.c2}  
+            <ModalDropdown style={styles.cajas} textStyle={styles.textoCaja}  
             dropdownStyle={styles.desplegable} options={frutas} 
             onSelect={(id, fruit) => setFruit(fruit)}/>
           
-        <Text style={styles.c}>
-          <Text style={styles.c}>Precio de la Fruta</Text>
+        <Text style={styles.titulos}>
+          <Text style={styles.titulos}>Precio de la Fruta</Text>
         </Text>
-        <TextInput style={styles.mdd2} keyboardType = {'number-pad'} onChangeText={price => setPrice(price)}/> 
+        <TextInput style={styles.cajas} keyboardType = {'number-pad'} onChangeText={price => setPrice(price)}/> 
         <TouchableOpacity  style={styles.boton} onPress={onPress}>
-          <Text style={styles.c3} >Añadir</Text>
+          <Text style={styles.textoBoton} >Añadir</Text>
         </TouchableOpacity>
     </View>
   );

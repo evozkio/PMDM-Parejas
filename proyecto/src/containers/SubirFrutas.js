84da16601bcf67730014cccb8d816561da2237b8
evozkio <39371612+evozkio@users.noreply.github.com>
2022-02-07 21:39:04 +0100

    arreglar validacion

--- proyecto/src/containers/SubirFrutas.js
+++ proyecto/src/containers/SubirFrutas.js
@@ -7,15 +7,13 @@ import styles from '../styles/styles';
 function SubirFrutas({navigation}){
   const frutas =['Piña', 'Manzana','Melocotón', 'Uvas','Naranja','Kiwi','Plátano','Pera'];
 
-  const [fruit,setFruit]=useState(null);
+  const [fruit,setFruit] = useState(null);
   const [price, setPrice] = useState(null);
 
   const onPress = () => {
-    console.log(fruit)
-    console.log(price)
-    if(isNaN(price)){
-      console.log("estoy")
-      Alert.alert("La fruta no se puede añadir")
+ 
+    if(isNaN(price) || fruit == null || price == null || price <= 0){
+      Alert.alert("La fruta no se puede añadir");
     }else{
       fetch('http://10.0.2.2:8080/fruits', {
         method: 'POST',
@@ -30,7 +28,7 @@ function SubirFrutas({navigation}){
       })
       .then((responseJson) => {
         console.log('getting data from fectch', responseJson);
-        Alert.alert("Fruta añadida correctamente")
+        Alert.alert("Fruta añadida correctamente");
         setFruit(null);
         setPrice(null);
       })
